c5c5e627de97f4d99696faf18865afcaacf19d50
fit666 <525188200@qq.com>
2019-06-17 14:42:22 +0800

    部署项目

---
+++ SpringBoot01-2/src/main/java/com/mj/main/Base.java
@@ -0,0 +1,9 @@
+package com.mj.main;
+
+public class Base {
+
+	public static void main(String[] args) {
+System.out.println();
+	}
+
+}
---
+++ SpringBoot01-2/src/main/java/com/mj/test/Base.java
@@ -0,0 +1,8 @@
+package com.mj.test;
+
+public class Base {
+	public static void main(String[] args) {
+		System.out.println("ss");
+	}
+
+}
