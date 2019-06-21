c26e0ba7e01ae86730caf6f6aa507ac869dda998
yanyu666666 <845572849@qq.com>
2019-06-21 09:33:21 +0800

    。。。。。

--- HeroHotel/src/main/java/com/hero/hotel/config/ShiroConfiguration.java
+++ HeroHotel/src/main/java/com/hero/hotel/config/ShiroConfiguration.java
@@ -9,7 +9,6 @@ import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.hero.hotel.realm.MyRealm;
 
 @Configuration
 public class ShiroConfiguration {
