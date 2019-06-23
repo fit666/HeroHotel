d50bc3bfb8f9ac27ab6b51acc2a1bfbb05f1fc89
yanyu666666 <845572849@qq.com>
2019-06-23 11:28:47 +0800

    财务查询 统计图

---
+++ HeroHotel/src/main/java/com/hero/hotel/dao/OrderDao.java
@@ -0,0 +1,27 @@
+package com.hero.hotel.dao;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface OrderDao {
+	// 1 表示 已入住订单 收入 日
+	@Select("select sum(total) from t_order where TO_DAYS(createtime) = TO_DAYS(#{daytime}) AND flag = 1")
+	public Double findMoneyDayIn(Date daytime);
+
+	// 2 表示 退款订单 退款 日
+	@Select("select sum(total) from t_order where TO_DAYS(createtime) = TO_DAYS(#{daytime}) AND flag = 2")
+	public Double findMoneyDayOut(Date daytime);
+
+	// 1 表示 已入住订单 收入 月
+	@Select("select sum(total) from t_order where date_format(createtime,'%Y-%m')=date_format(#{Monthtime},'%Y-%m') AND flag = 1")
+	public Double findMoneyMonthIn(String Monthtime);
+
+	// 2 表示 退款订单 退款 月
+	@Select("select sum(total) from t_order where date_format(createtime,'%Y-%m')=date_format(#{Monthtime},'%Y-%m') AND flag = 2")
+	public Double findMoneyMonthOut(String Monthtime);
+
+	
+	
+}
