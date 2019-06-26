2dc06acac4b2d77eab26b76ab60248d988c3bb22
yanyu666666 <845572849@qq.com>
2019-06-26 16:13:27 +0800

    houseDao  优化修改

--- HeroHotel/src/main/java/com/hero/hotel/dao/HouseDao.java
+++ HeroHotel/src/main/java/com/hero/hotel/dao/HouseDao.java
@@ -15,14 +15,8 @@ public interface HouseDao {
 	@Select("select * from t_House")
 	public List<House> findRoomAllStatus();
 
-	@Select("select * from t_House where flag=0")
-	public List<House> findOkRooms();
-
-	@Select("select * from t_House where flag=1")
-	public List<House> findNoRooms();
-
-	@Select("select * from t_House where flag=2")
-	public List<House> findZangRooms();
+	@Select("select * from t_House where flag=#{flag}")
+	public List<House> findRooms(Integer flag);
 
 	@Select("select roomid from orderinfor where today=#{time} and flag=0")
 	public List<Integer> findOrderInforRid(String time);
--- HeroHotel/src/main/java/com/hero/hotel/service/impl/HouseServiceImpl.java
+++ HeroHotel/src/main/java/com/hero/hotel/service/impl/HouseServiceImpl.java
@@ -32,17 +32,17 @@ public class HouseServiceImpl implements HouseService {
 
 	@Override
 	public List<House> findOkRooms() {
-		return houseDao.findOkRooms();
+		return houseDao.findRooms(0);
 	}
 
 	@Override
 	public List<House> findNoRooms() {
-		return houseDao.findNoRooms();
+		return houseDao.findRooms(1);
 	}
 
 	@Override
 	public List<House> findZangRooms() {
-		return houseDao.findZangRooms();
+		return houseDao.findRooms(2);
 	}
 
 //	 @Override
